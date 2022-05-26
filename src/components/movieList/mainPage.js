aac3eba122f9cfef8f56a70f3903049fdfdbdef5
Medal17 <user@Medal.local>
2022-05-26 18:42:56 +0100

    adds view movie ui

--- src/components/movieList/mainPage.js
+++ src/components/movieList/mainPage.js
@@ -4,19 +4,21 @@ import SideBarCard from '../cards/sideBarCard';
 import HeadText from '../HeadText';
 import SearchBar from './searchBar';
 
-export default function MainPage() {
+export default function MainPage(props) {
+  // console.log(props.data) 
   return (
-    <Grid item px={2}>
+    <Grid item md={9} px={2}>
       <HeadText text='Now Showing' />
       <SearchBar/>
       <Grid container >
-        <Grid item md={6} sm={6} xs={12}>
-          <SideBarCard/>
-        </Grid>
+        { props.data?.Search.map(e=>
+          <Grid item md={4} sm={6} xs={12}>
+          <SideBarCard data={e}/>
+        </Grid>)}
 
-        <Grid item md={6} sm={6} xs={12}>
+        {/* <Grid item md={6} sm={6} xs={12}>
         <SideBarCard/>
-        </Grid>
+        </Grid> */}
       </Grid>
     </Grid>
   );
--- src/pages/ViewMovie.js
+++ src/pages/ViewMovie.js
@@ -1,11 +1,30 @@
-import { Grid } from '@mui/material';
+import { Grid, Typography, Chip } from '@mui/material';
 import React from 'react';
+import HeadText from '../components/HeadText';
 
 export default function ViewMovie() {
   return (
-    <Grid container>
-        <Grid container>
-          <img src='logo512.png'/>
+    <Grid container justifyContent='center' py={2}>
+        {/* //Movie Image */}
+        <Grid container justifyContent='center' md={10} sm={10} xs={11} sx={{bgcolor:'rgba(255,255,255,0.1)', borderRadius:'15px'}}>
+          <img src='logo512.png' height={250}/>
+        </Grid>
+
+        {/* Movie Information */}
+        <Grid mx={{xs:1, md:8, sm:5}} md={10} sm={10} xs={11}  my={2} container>
+          <Grid container justifyContent={'space-between'}>
+            <HeadText text='Hello World Movie'/>
+            {/* <HeadText bodyText text='Time'/> */}
+            <Chip label='01:10:30' sx={{bgcolor:'#fff'}}/>
+          </Grid>
+
+          <Grid container>
+            <HeadText bodyText text='body'/>
+          </Grid>
+
+          <Grid container>
+            <HeadText bodyText text='body'/>
+          </Grid>
         </Grid>
     </Grid>
 
